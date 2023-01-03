833808cf93297cab6ce7349b7787681ed594057e
xjwl0305 <xjwl0305@gmail.com>
2023-01-03 17:24:36 +0900

    Fix database timezone KST

--- models/index.js
+++ models/index.js
@@ -10,9 +10,37 @@ const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  //sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  sequelize = new Sequelize(  process.env.database,
+      process.env.username,
+      process.env.password,
+      {
+        host: process.env.host,
+        dialect: "mysql",
+        timezone: "+09:00", // DB에 저장할 때 시간 설정
+          dialectOptions: {
+              charset: 'utf8mb4',
+              dateStrings: true,
+              typeCast: true
+          }
+          ,define: {
+              timestamps: true
+          }
+      });
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(config.database, config.username, config.password,       {
+      host: config.host,
+      dialect: "mysql",
+      timezone: "+09:00", // DB에 저장할 때 시간 설정
+      dialectOptions: {
+          charset: 'utf8mb4',
+          dateStrings: true,
+          typeCast: true
+      }
+      ,define: {
+          timestamps: true
+      }
+  });
 }
 
 fs
