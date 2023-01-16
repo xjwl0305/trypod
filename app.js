421d9762160caae15a3a94e268688847586d48b4
xjwl0305 <xjwl0305@gmail.com>
2023-01-16 15:20:19 +0900

    Update management itemGetAll

--- app.js
+++ app.js
@@ -45,12 +45,6 @@ app.use('/', indexRouter);
 app.use(function(req, res, next) {
   next(createError(404));
 });
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
 // error handler
 // app.use(function(err, req, res, next) {
 //   // set locals, only providing error in development
