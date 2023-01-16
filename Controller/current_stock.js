21fc5ae405c13c934ad3b50980de2a1f3d6040cb
xjwl0305 <xjwl0305@gmail.com>
2023-01-16 14:01:08 +0900

    xc

--- Controller/current_stock.js
+++ Controller/current_stock.js
@@ -175,13 +175,20 @@ exports.reportDownload = async (req, res) => {
     const uid = req.query.uid;
     const date = req.query.date;
     const select = req.query.select;
-    const data = req.query.data;
     const currentDate = new Date();
     const currentDayFormat = currentDate.getFullYear()+"-"+
         (currentDate.getMonth()+1)+"-"+
         currentDate.getDate();
-    if(select === 1) { // 아이템별 보고서 다운로드
+    if(Number(select) === 1) { // 아이템별 보고서 다운로드
+        const standard = await sequelize.query('select summary.id as summary_id from (select max(summary.created_at) as standard_date from summary left join user u on u.id = summary.user_id where u.id = :uid and DATE(summary.created_at) = :date) as t2, summary left join user u on u.id = summary.user_id\n'+
+        'where u.id = :uid and DATE(summary.created_at) = :date and t2.standard_date = summary.created_at', {replacements: { uid: uid, date:date}, type: QueryTypes.SELECT});
 
+        const data = await sequelize.query("select item_name, item_category, item_code, sum(weight), sum(usage_weight), branch_name, layer_name, warehouse_name, GROUP_CONCAT(device_number order by device_number SEPARATOR ',') as device_numbers\n"+
+        "from summary_content left join summary s on s.id = summary_content.summary_id where s.id = :id group by item_name", {replacements: {id:standard[0].summary_id}, type: QueryTypes.SELECT});
+
+        const connection = await sequelize.query("select GROUP_CONCAT(device_number) as device_number, item_name from summary_content where summary_id = 361 and connection = 'warning' group by item_name", {replacements: {id:standard[0].summary_id}, type: QueryTypes.SELECT});
+
+        const a = 1;
     }else{ // 디바이스별 보고서 다운로드
         const data = await sequelize.query('select e.device_number, i.name, i.category, i.code, drd.weight, c.weight as container_weight, drd.battery, l.branch_name, l.layer_name, l.warehouse_name, drd.data_interval, drd.created_at from (select earlivery_device_id, max(created_at) as max_date from device_raw_data group by earlivery_device_id) as t2, device_raw_data drd\n' +
             'left join earlivery_device e on drd.earlivery_device_id = e.id left join item i on i.id = e.item_id left join location l on e.location_id = l.id left join container c on c.id = e.container_id left join user u on l.user_id = u.id\n' +
