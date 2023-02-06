340bb69c57b7fd19a831c95f5759af48a50238b6
xjwl0305 <xjwl0305@gmail.com>
2023-02-06 17:14:32 +0900

    Update CORS option

--- app.js
+++ app.js
@@ -12,7 +12,11 @@ const schedule = require("node-schedule");
 const request = require("request-promise-native");
 
 const app = express();
-app.use(cors());
+const corsOptions = {
+    exposedHeaders: 'Authorization'
+}
+
+app.use(cors(corsOptions));
 sequelize.sync({ force: false }) // 서버 실행시마다 테이블을 재생성할건지에 대한 여부
     .then(() => {
       console.log('데이터베이스 연결 성공');
