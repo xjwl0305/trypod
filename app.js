7b42724bf8bb96c62936020c5d9df43b2bcd8219
xjwl0305 <xjwl0305@gmail.com>
2023-02-08 14:10:00 +0900

    Update check email code

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const request = require("request-promise-native");
 
 const app = express();
 const corsOptions = {
-    origin: true,
+    origin: "http://3.34.196.3:3302",
     exposedHeaders: 'Report_title',
     credentials: true
 }
