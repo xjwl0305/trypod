24d65fb8aace1ef5ec2349d2d5f317662d983d93
xjwl0305 <xjwl0305@gmail.com>
2023-02-10 10:52:06 +0900

    Update ReportSetting function

--- DB/current_stock.js
+++ DB/current_stock.js
@@ -505,13 +505,8 @@ exports.ReportTimeSetting = async (uid, account, base_time, report_writing_cycle
 
     const result = {};
 
-    await axios.post(`http://localhost:8000/sched_change`, {
-        start_time: base_time,
-        writing_cycle:report_writing_cycle,
-        account: account,
-        uid: uid
-    },{
-        headers: {'Content-Type' : 'application/json'}
+    await axios.post(`http://3.34.196.3:8000/sched_change?start_time=`+base_time+'&writing_cycle='+report_writing_cycle+'&account='+account+'&uid='+uid, {
+        headers: {'Content-Type': 'application/json'}
     }).then(function (res) {
         console.log(res);
         if (res.status === 200) {
